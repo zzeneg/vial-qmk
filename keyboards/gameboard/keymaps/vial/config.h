6da5ae6bb0cf7274b3a53e1a7ccc5b1b3bb9b717
Evgenii Vilkov <zzeneg@gmail.com>
2023-09-03 14:36:33 +0200

    add gameboard

---
+++ keyboards/gameboard/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 zzeneg (@zzeneg)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID { 0xF6, 0xFA, 0xD8, 0x61, 0xDE, 0xD5, 0x4A, 0x86 }
+
+/* VIAL secure unlock keystroke - currently two outer thumb keys */
+#define VIAL_UNLOCK_COMBO_ROWS { 4, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 6 }
